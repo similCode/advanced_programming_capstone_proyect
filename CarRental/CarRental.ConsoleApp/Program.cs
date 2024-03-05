bbe4ef8a5d18b8cab294a2e6534e7c365f14e84b
Leo <leandrosimil11@gmail.com>
2024-03-05 09:58:25 -0500

    CRUD finished

--- CarRental/CarRental.ConsoleApp/Program.cs
+++ CarRental/CarRental.ConsoleApp/Program.cs
@@ -31,7 +31,7 @@ internal class Program
         
         // Crear un usuario
 
-         Users user = new("Carlos","Rodríguez","99062207946","Cuba"UserRoleType.Guest);
+         Users user = new("Carlos","Rodríguez","99062207946","Cuba",UserRoleType.Guest);
          appContext.Set<Users>().Add(user);
          appContext.SaveChanges();
         
@@ -63,11 +63,60 @@ internal class Program
           
         }
         appContext.SaveChanges();
-      
+
+
+
+        //Creando Insurance
+        Insurance carInsurance = new Insurance("enable","4394925832",new DateTime(2029,3,12), new DateTime(2023,5,12));
+        appContext.Set<Insurance>().Add(carInsurance);
+        appContext.SaveChanges()
+
+        //leyendo insurance
+        var insurances = appContext.Set<Insurances>().ToList();
+
+        //actualizando insurance
+        foreach(insurance in insurances)
+        {
+            insurance.Status = "disable";
+            appContext.Set<Insurance>().Update(insurance)
+        }
+        appContext.SaveChanges()
+
+
+
+        //Borrando insurance
+        foreach (insurance in insurances)
+        {
+            appContext.Set<Insurance>().Remove(insurance)
+        }
+        appContext.SaveChanges()
+
+
+        //Creando Somaton
+        Somaton carSomaton = new Somaton(new DateTime(2028,12,12), new DateTime(2023,8,9),"Approved",09310940748658516506);
+        
+        //Leyendo Somaton
+        var somatons = appContext.Set<Somaton>().ToList();
+
+        //Actualizando Somaton
+        foreach (somaton in somatons)
+        {
+            somaton.Status = "Cancelled";
+            appContext.Set<Somaton>().Update(somaton)
+        }
+        appContext.SaveChanges()
+
+        //Borrando Somaton
+        foreach (somaton in somatons)
+        {
+            appContext.Set<Somaton>().Remove(somaton)
+        }
+        appContext.SaveChanges()
+
 
     }
 
-     
+
 
 
 
