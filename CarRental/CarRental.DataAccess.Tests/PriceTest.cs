618ebf61a4aa9754375b58bf15e6c5011cf95707
Leo <leandrosimil11@gmail.com>
2024-03-16 16:14:33 -0400

    adding test initial ommit

---
+++ CarRental/CarRental.DataAccess.Tests/PriceTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Tests
+{
+    [TestClass]
+    public class PriceTest
+    {
+    }
+}
---
+++ CarRental/CarRental.DataAccess.Tests/Utilities/ConnectionStringProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Tests.Utilities
+{
+    public static class ConnectionStringProvider
+    {
+        public static string GetConnectionString() => "Data Source=Data.sqlite";
+    }
+}
